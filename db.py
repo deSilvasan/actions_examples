f8dc9cf82b98aee9fd1c466d128692822d8fb114
albertgreinoecker <albert.greinoecker@gmail.com>
2025-05-24 10:45:51 +0200

    Update db.py: just to check if db connection works

--- db.py
+++ db.py
@@ -1,1 +1,18 @@
+import mysql.connector
+
+def get_mysql_version():
+    conn = mysql.connector.connect(
+        host="db",
+        user="root",
+        password="example",
+        database="testdb"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT VERSION();")
+    print("MySQL version:", cursor.fetchone()[0])
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    get_mysql_version()
 
