0bbd8d1d4889aa82ac419b66f37d2aee9be4146d
albertgreinoecker <albert.greinoecker@gmail.com>
2025-05-25 12:50:43 +0200

    Create test_db.py

---
+++ app/test_db.py
@@ -0,0 +1,9 @@
+class TestMySQLVersion(unittest.TestCase):
+
+    def test_get_mysql_version(self):
+        version = get_mysql_version()
+        self.assertIsInstance(version, str)
+        self.assertRegex(version, r'^\d+\.\d+\.\d+')  # z.B. 8.0.33
+
+if __name__ == "__main__":
+    unittest.main()
